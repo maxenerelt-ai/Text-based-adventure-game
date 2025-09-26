cbeb551c5047588db34862a431bf638ff2f3b460
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-26 19:36:44 +0000

    Co-authored-by: dharrmanfam <sushidontlie520@users.noreply.github.com> Co-authored-by: Tranza2679 <Tranza2679@users.noreply.github.com> Co-authored-by: sushidontlie520 <dharrmanfam@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,23 +1,86 @@
 # ME CC SN SP 6th DND final
 
+# Stats for each class
+import random
+
+strength = 0 
+speed = 0 
+defense = 0
+health = 0
+ki = 0
+arcane = 0
+
+
 
 #Variables
 print("The mage is a wizard, the monk is a martial artist, and the barbarian is a strong man with a weapon.")
-game_class = input("Welcome to our quest, what charcater do you wanna play as? Options are: Mage, Monk, Barbarian.")
-if game_class == Mage 
+game_class = input("Welcome to our quest, what charcater do you wanna play as? Options are: Mage, Monk, Barbarian.").title().strip()
+
+if game_class == "Mage":
+  print("Good choice!") 
+
+
+  arcane = 20
+  speed = 15
+  defense = 15
+  health = 20  
+  
+  print("You are a mage, you start out in a forest after accepting a quest to go to a dungeon")
+
+elif game_class == "Monk":
+    print("Good choice!")
+    print("You are a monk, you enter a forest to take re")
+
+
+    ki = 20
+    speed = 20
+    defense = 10
+    health = 20 
+    
+elif game_class == "Barbarian":
+    print("Good choice!")
+    print("You are a Barbarian!")
+
+else:
+
+    strength = 25
+    speed = 15
+    defense = 20
+    health = 10
+    print("Please select a character to play!")
+    
 
 
 
+    if game_class == "Mage":
+        print("To begin your quest please select the path you want to go through your options are either the left path (a dusty dry path leading to the desert) or the right path (A broken down path with cobwebs)")
+        path = input("What path will you take?").title()
+    if path == "Left":
+        print("You enter a dry desert but something catches your eye; a bright lively town.")
+        town = input("Do you go towards the town or go back to the forest to go on the right path?").strip()
+    elif path == town:
+        print("You go towards the town and they greet you with water and food! And also they instruct you that the dungeon is down the broken down path")
+        print("You go down the broken down path and see the dungeon!")
+    else:
+        print("You go down the right path with all the broken down pavement and cobwebs and come upon the dungeon!")
+        
 
+    print("Now that you start, your options are a rainforest or a cave.")
 
+elif game_class == "Barbarian":
+    print("Your path choices are ____ or ____")
 
-
-
-
-
-
-
-
+else:
+print("Sorry, that is not a actual option.") 
+    g
+o
+i
+ng
+ 
+t
+hr
+ough 
+a dark cave
 
 
 
