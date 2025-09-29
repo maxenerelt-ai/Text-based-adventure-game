729105321d7ae17a63498b7abfd1a19e1506d30c
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-29 19:41:37 +0000

    tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,15 @@
-# ME CC SN               6th Tic Tac Toe
+# ME CC SN SP 6th Tic Tac Toe
 
+import random
 # Variables
+user_input = input("Pick either O or X for a Tic Tac Toe game!: ").capitalize()
+
+if user_input == "X":
+    print("You picked X!")
+elif user_input == "O":
+    print("You picked O!")
+else:
+    print("You picked neither O or X, please pick O or X PLEASE!")
+
+
 
