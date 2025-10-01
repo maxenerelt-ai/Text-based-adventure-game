a643297c6e633231eb0ecfbd258426246522a20b
dharrmanfam <charlie.cogswell@ucas-edu.net>
2025-09-30 22:30:29 -0600

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -84,29 +84,51 @@ while game_end == False:
 
 #winning combos Charlie 
 def winner_combos(player): 
-    win_combos = []
-    [0,1,2], [3,4,5], [6,7,8],  #if_row
-    [0,4,8], [2,4,6], #if_diagonal
-    [0,3,6], [1,4,7], [2,5,8] #if_column
+    win_combos = [
+        [0,1,2], [3,4,5], [6,7,8],  # rows
+        [0,3,6], [1,4,7], [2,5,8],  # columns
+        [0,4,8], [2,4,6]            # diagonals
+    ]
     for combo in win_combos:
-            if all (board[i] == player for i in combo):
-                return True 
-            return False
-    if combo == [0,1,2]:
-        if [0,1,2] == user_input:
-
- 
-          
-
-
-
-
-
-
-            
-
-    
-
-
-
-
+        if all(board[i] == player for i in combo):
+            return True
+    return False
+while game_end == False: 
+    if turns % 2 == 1 and user_input == "X":
+        game_board()
+        player_choice = int(input("Choose a number from 1 to 9?"))
+        if 1 <= player_choice <= 9 and player_choice in numbers_avaliable:
+            change_board(player_choice, user_input) 
+            numbers_avaliable.remove(player_choice)
+            if winner_combos(user_input):
+                game_board()
+                print("You win!")
+                game_end = True
+            turns += 1
+#sophie
+    elif turns % 2 == 1 and user_input == "O":
+        game_board()
+        player_choice = int(input("Choose a number from 1 to 9?"))
+        if 1 <= player_choice <= 9 and player_choice in numbers_avaliable: 
+            change_board(player_choice, user_input)
+            numbers_avaliable.remove(player_choice)
+            if winner_combos(user_input):
+                game_board()
+                print("You win!")
+                game_end = True
+            turns += 1
+#sophie     
+    else:
+        game_board()
+        ai_pick = random.choice(numbers_avaliable)
+        change_board(ai_pick, ai_choice)
+        numbers_avaliable.remove(ai_pick)
+        if winner_combos(ai_choice):
+            game_board()
+            print("ai wins!")
+            game_end = True
+        turns +=1
+    if turns > 9: 
+        game_board()
+        print("It's a tie!") 
+        game_end = True
